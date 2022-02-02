7fcd6d02ca1f19b368eb05f91515576886e73658
Mark Stouffer <1802850+InTEGr8or@users.noreply.github.com>
2022-02-01 22:43:15 -0800

    updates

---
+++ bob/Bob.fs
@@ -0,0 +1,21 @@
+﻿module Bob
+open System.Text.RegularExpressions
+
+
+let Match pattern input =
+    let m = Regex.Match(input, pattern) in
+    m.Success && m.Length > 0
+
+let response (input: string): string = 
+    match input with
+    | i when i.Trim() = "" -> "Fine. Be that way!"
+    | i when Match "[a-zA-Z]" i 
+        |> not
+        && i.Trim().EndsWith("?") -> "Sure."
+    | i when Match "[a-zA-Z]" input 
+        |> not -> "Whatever."
+    | i when i.ToUpper() = i 
+        && i.Trim().EndsWith("?") -> "Calm down, I know what I'm doing!"
+    | i when i.Trim().EndsWith("?") -> "Sure."
+    | i when i.ToUpper() = i -> "Whoa, chill out!"
+    | _ -> "Whatever."
